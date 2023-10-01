fdb7add4e0658944d9b3689c2163752e8c3405e4
JeDeveloppe <jedeveloppe.contact@gmail.com>
2023-10-01 22:32:48 +0200

    ok start member templates

---
+++ src/Controller/Member/MemberController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller\Member;
+
+use App\Entity\User;
+use App\Form\AccountType;
+use App\Repository\ContactRepository;
+use App\Repository\PaiementRepository;
+use App\Service\MeetingService;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/member', name: 'member_')]
+class MemberController extends AbstractController
+{
+    public function __construct(
+        private Security $security,
+        private PaiementRepository $paiementRepository,
+        private ContactRepository $contactRepository,
+        private MeetingService $meetingService,
+        private EntityManagerInterface $entityManagerInterface
+    )
+    {
+    }
+
+    #[Route('/dashboard', name: 'dashboard')]
+    public function dashboard(): Response
+    {
+
+        $user = $this->security->getUser();
+
+        return $this->render('member/dashboard.html.twig', [
+            'paiements' => $this->paiementRepository->findBy(['name' => $user]),
+            'questions' => $this->contactRepository->findBy(['user' => $user], ['id' => 'DESC'], 5),
+            'donneesMeeting' => $this->meetingService->nextMeetingCalc()
+        ]);
+    }
+
+    #[Route('/mon-compte', name: 'account')]
+    public function account(Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $user = $this->security->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+
+            $this->entityManagerInterface->persist($user);
+            $this->entityManagerInterface->flush();
+
+
+
+
+            $this->addFlash('success', 'Données mises à jour !');
+        }
+
+        return $this->render('member/account.html.twig', [
+            'accountForm' => $form->createView(),
+            'donneesMeeting' => $this->meetingService->nextMeetingCalc()
+        ]);
+    }
+}
---
+++ src/Form/AccountType.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Job;
+use App\Entity\Shop;
+use App\Entity\User;
+use App\Entity\SexStatus;
+use Doctrine\ORM\QueryBuilder;
+use App\Repository\JobRepository;
+use App\Repository\ShopRepository;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+class AccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email:',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            // ->add('password', PasswordType::class, [
+            //     'label' => 'Mot de passe:',
+            //     'attr' => [
+            //         'class' => 'form-control',
+            //         'readonly' => true 
+            //     ]
+            // ])
+            ->add('phone', TelType::class, [
+                'label' => 'Téléphone:',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => 'Nom:',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'label' => 'Prénom:',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('job', EntityType::class, [
+                'label' => 'Poste:',
+                'class' => Job::class,
+                'query_builder' => function (JobRepository $er): QueryBuilder {
+                    return $er->createQueryBuilder('j')
+                        ->orderBy('j.name', 'ASC');
+                },
+                'placeholder' => 'Faire un choix...',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('shop', EntityType::class, [
+                'label' => 'Lieu de rattachement:',
+                'class' => Shop::class,
+                'query_builder' => function (ShopRepository $er): QueryBuilder {
+                    return $er->createQueryBuilder('s')
+                        ->orderBy('s.counterMark', 'ASC');
+                },
+                'choice_label' => function ($shop) {
+                    return $shop->getCounterMark().' - '.$shop->getName();
+                },
+                'placeholder' => 'Faire un choix...',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('sex', EntityType::class, [
+                'label' => 'Genre:',
+                'class' => SexStatus::class,
+                'placeholder' => 'Faire un choix...',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
